c89cfa9dce623b1f6d4ace6a6e1a2b4138a7ed4d
Cesar <ipog71@gmail.com>
2022-02-25 12:28:33 -0600

    Ejercicios

---
+++ Ejercicios/Ejercicio 1.py
@@ -0,0 +1,6 @@
+a = float(input("Ingrese el valor de a: "))
+b = float(input("Ingrese el valor de b: "))
+c = float(input("Ingrese el valor de c: "))
+resultado = (a**3 * ((b**2) - (2 * a * c))) / (2*b)
+
+print("El resultado es", resultado)

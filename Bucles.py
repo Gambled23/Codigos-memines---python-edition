1cfdaba1193e881d29b9ad9fcadbe9a8a682b266
Cesar <ipog71@gmail.com>
2022-02-25 12:46:29 -0600

    Un while xd

---
+++ Bucles.py
@@ -0,0 +1,7 @@
+elias = int(input("Cuantas veces imprimo elias? "))
+pablo = int(input("Cuantas veces imprimo Pablo? "))
+eliasJoto = int(input("Cuantas veces imprimo eliasJoto? "))
+i = 0
+while i < elias:
+    print("Elias")
+    i+=1

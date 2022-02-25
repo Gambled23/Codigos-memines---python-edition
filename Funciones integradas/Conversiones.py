bf76e3fe9481aac31d79fc6ecb5881804e18d983
Cesar <ipog71@gmail.com>
2022-02-25 12:23:22 -0600

    Funciones de conversiones

--- Funciones integradas/Conversiones.py
+++ Funciones integradas/Conversiones.py
@@ -1,17 +1,21 @@
 import numbers
 
-#Convertir de numero a cadena
+# Convertir de numero a cadena
 num = str(5.3)
-#Convertir de decimal a binario
+# Convertir de decimal a binario
 numBin = bin(6)
-#Convertir de decimal a hexadecimal
+# Convertir de decimal a hexadecimal
 numHex = hex(32)
-#Convertir de binario a decimal
-numBinDec = int(0b1100) #el 0b indica que es binario
-#Redondear
+# Convertir de binario a decimal
+numBinDec = int(0b1100)  # el 0b indica que es binario
+# Redondear
 numRed = round(5.7)
+# Longitud de una cadena
+cadena1 = len("Pablo")
+
 print(num)
 print(numBin)
 print(numHex)
 print(numBinDec)
 print(numRed)
+print("La cadena Pablo tiene ", cadena1, "Caracteres")

3de76fd8f06cccb8e506647e8cb518a03acb98fb
Cesar <ipog71@gmail.com>
2022-02-25 12:02:09 -0600

    Basicos de piton

---
+++ Basicos/Formas de imprimir.py
@@ -0,0 +1,16 @@
+edad = 19
+nombre = "pablo"
+
+#1ra forma, con el print y concatenando las cadenas como en java 
+print("Hola",nombre,"tienes",edad,"anios")
+
+#2da forma, con la funcion format
+print("hola {} tienes {} anios".format(nombre, edad))
+
+#3ra forma, usando el f al inicio y las variables entre corchetes, es de las mas sencillas
+print(f"Hola {nombre} tienes {edad} anios")
+
+'''
+Todas dan el mismo resultado
+(also comentario de varias lineas xd)
+'''
---
+++ Basicos/mamadas.py
@@ -0,0 +1,8 @@
+num1 = 10
+num2 = 1.254
+resultado = (num1 + num2) /2
+
+print("El resultado es", resultado)
+
+resultado = "pablo"
+print("Ahora es una string, mira: ", resultado)

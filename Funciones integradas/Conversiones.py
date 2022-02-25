1f48f8abc6392832fd748bb2b6dc80cde0539448
Cesar <ipog71@gmail.com>
2022-02-25 12:19:51 -0600

    External terminal

---
+++ Funciones integradas/Conversiones.py
@@ -0,0 +1,17 @@
+import numbers
+
+#Convertir de numero a cadena
+num = str(5.3)
+#Convertir de decimal a binario
+numBin = bin(6)
+#Convertir de decimal a hexadecimal
+numHex = hex(32)
+#Convertir de binario a decimal
+numBinDec = int(0b1100) #el 0b indica que es binario
+#Redondear
+numRed = round(5.7)
+print(num)
+print(numBin)
+print(numHex)
+print(numBinDec)
+print(numRed)

22aba58dba2ab935b18b5e852d5c1f3c28eb3ac7
Cesar <ipog71@gmail.com>
2022-02-25 12:34:24 -0600

    a

---
+++ if else.py
@@ -0,0 +1,9 @@
+n1 = int(input("Digite n1: "))
+
+#No hace falta usar llaves, la identacion son las llaves
+if n1 > 0:
+    print("El numero es positivo")
+elif n1 == 0:
+    print("Joder es un cero")
+else:
+    print("El numero es negativo")

27978c08a728f40d384842c332ab3d356f9f06ca
Cesar <ipog71@gmail.com>
2022-02-25 12:10:36 -0600

    Entrada de datos

---
+++ Basicos/Entrada de datos.py
@@ -0,0 +1,10 @@
+
+#Para guardar tipos de datos cadena
+nombre = input("Digita tu nombre: ")
+
+#Para guardar valores int
+edad = int(input("Digita tu edad: "))
+
+#Para guardar valores float
+numFloat = float(input("Digita algun float xd: "))
+print(f"Hola {nombre}, tienes {edad} anios, e ingresaste el {numFloat} solo pq si")
